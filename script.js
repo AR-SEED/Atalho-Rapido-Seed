979955caeb9e2a4a5801f49e98afe07db14e1441
Gustavo Henrique <135024074+AR-SEED@users.noreply.github.com>
2023-06-07 11:10:28 -0300

    (INULTILIDADE)
    SEM FUNÇÃO.

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const buttons = document.querySelectorAll('.btn');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const href = button.dataset.href;
-    window.location.href = href;
-  });
-});
-
+.
